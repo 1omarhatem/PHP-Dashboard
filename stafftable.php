89b4d5df7f085f58ccf56510aa1e86231bf698a3
Omar Hatem <113442544+1omarhatem@users.noreply.github.com>
2024-11-18 20:40:16 +0200

    V5

---
+++ stafftable.php
@@ -0,0 +1,17 @@
+<!-- table -->
+<table class="table" id="userstable">
+  <thead>
+    <tr>
+      <th scope="col">Picture</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Major</th>
+      
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+
+  </tbody>
+</table>
+<!-- table -->
